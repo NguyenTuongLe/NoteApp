ddddb18082718f9514122ff72a49fca702fd4030
unknown <hathinga07@gmail.com>
2021-05-31 15:53:32 +0700

    change app coler

--- app/src/androidTest/java/com/sun/noteapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/sun/noteapp/ExampleInstrumentedTest.kt
@@ -14,6 +14,11 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
@@ -23,3 +28,5 @@ class ExampleInstrumentedTest {
         assertEquals("com.sun.noteapp", appContext.packageName)
     }
 }
+
+
